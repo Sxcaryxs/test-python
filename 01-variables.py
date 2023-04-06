afaf8af4acffc0b254cd9003029da9f204b26e25
Ignacio Leal <matiasleal147@gmail.com>
2023-04-06 14:18:23 -0400

    Mi primer commit

---
+++ 01-variables.py
@@ -0,0 +1,23 @@
+#Comentario de pana#
+print ("Hola mundo")
+
+Nombre= "Ignacio"
+
+print (Nombre)
+print("Hola soy ",Nombre)
+
+edad= 18
+
+print("Mi edad es: ",edad,"años")
+
+print("Hola mi nombre es: "+Nombre+"Y tengo "+str(edad)+" años")
+
+Nombre="Duki"
+
+print("Hola mi nuevo nombre es ",Nombre)
+
+Nombre=input("Escribe tu nombre ") 
+
+nombre1=input("Escribe tu otro nombre \n")
+
+print("Tu nombre es ",nombre1)
