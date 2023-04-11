91c8421c46103ede5c1580c4dceefe773d825562
Ignacio Leal <matiasleal147@gmail.com>
2023-04-11 14:35:56 -0400

    Cambio

--- 01-variables.py
+++ 01-variables.py
@@ -12,6 +12,8 @@ print("Mi edad es: ",edad,"años")
 
 print("Hola mi nombre es: "+Nombre+"Y tengo "+str(edad)+" años")
 
+print(f"hola soy {Nombre} y tengo {edad} años ")
+
 Nombre="Duki"
 
 print("Hola mi nuevo nombre es ",Nombre)
@@ -21,6 +23,9 @@ Nombre=input("Escribe tu nombre ")
 nombre1=input("Escribe tu otro nombre \n")
 
 print("Tu nombre es ",nombre1)
+
+ciudad,region,pais,año="Pto montt","Los lagos","Chile",2010
+
 
 
 
