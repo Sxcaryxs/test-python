969e33cf423b568a8d52759150c50a235d37a6b9
Ignacio Leal <matiasleal147@gmail.com>
2023-04-11 15:00:49 -0400

    Cambio4

--- 02-tiposdatos.py
+++ 02-tiposdatos.py
@@ -1,4 +1,7 @@
+
 estatura= 1.70
+
+edad=18
 
 peso=68
 
@@ -22,11 +25,50 @@ print(("La asignatura de ",asignatura,"es parte de la carrera de ",carrera))
 
 
 
-
-
-
 #Vectores
 
 estudiantes=["Ignacio","Pedro" ,"Juan" ,"Alonso"]
 
 print(estudiantes)
+
+estatura=1.72
+peso=60
+complejo= 1+4j
+
+print ("Mi estatura es de {estatura} y mi peso es de {peso}")
+
+print("Impresion de un numero complejo")
+
+print("Transformar un valor real a antero",int(peso))
+
+print("Transformar entero a real",float(edad))
+
+imc=peso / (estatura**2)
+
+print("Mi mci es de: ",imc,"n/")
+
+print("Mi IMC es de {:..2f}".format(imc),"n/")
+
+print("El largo de la palabra",asignatura,"es de: ",len(asignatura))
+print("El largo de la palabra ",carrera,"Es de : ",len(carrera))
+
+ampolleta= False
+interruptor= True
+
+print("La variable ampolleta es de tipo ",type)
+
+nueva_lista = list()
+
+print("Esta es una lista vacia: ",nueva_lista)
+
+otra_lista = []
+
+print(type(otra_lista))
+
+estudiantes = ["Ricardo","Alonso","Rodolfo","Cristian"]
+
+num = [1,2,3,4,5,6]
+
+lenguaje= ["python"]
+
+Lista_mixta = [str("xd"),float(1)]
