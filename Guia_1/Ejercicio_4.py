05989a944503193239b02ce26adb0dcf561c541a
Ignacio Leal <matiasleal147@gmail.com>
2023-05-16 19:54:12 -0400

    Guardar

---
+++ Guia_1/Ejercicio_4.py
@@ -0,0 +1,16 @@
+nom1=input("Nombre ")
+nom2=input("Nombre de otra persona ")
+
+nombre1=list(nom1)
+nombre2=list(nom2)
+
+if nombre1[0]==nombre2[0]:
+    print("La primera letra de los nombres coinciden")
+
+if nombre1[-1]==nombre2[-1]:
+    print("la ultima letra de los nombres coinciden")
+
+else:
+    print("Ningun nombre coincide")
+    
+
