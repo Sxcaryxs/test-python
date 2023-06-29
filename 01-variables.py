15cdbe57dcc8f58330da441fa5631ab2e2d1f1b3
Ignacio Leal <matiasleal147@gmail.com>
2023-06-28 20:47:21 -0400

    repositorio f

--- 01-variables.py
+++ 01-variables.py
@@ -1,31 +1,44 @@
-#Comentario de pana#
-print ("Hola mundo")
 
-Nombre= "Ignacio"
+#Un simple print
+print("Hola soy Diego")
 
-print (Nombre)
-print("Hola soy ",Nombre)
+#01-DECLARANDO VARIABLES DE TIPO CADENA DE TEXTO
+nombre = "Abel makkonen tesfaye"
+name = "Austin post"
 
-edad= 18
+#02-IMPRESION DE UNA VARIABLE
+print(name)
+print("Hola soy",name)
 
-print("Mi edad es: ",edad,"años")
+#Declarando una tercera variable de tipo númerico
+edad = 29
 
-print("Hola mi nombre es: "+Nombre+"Y tengo "+str(edad)+" años")
+#03-IMPRESION DE TEXTO + VARIABLE (IMPRESIÓN VARIABLE EDAD)
+print("Mi edad es de", edad)
 
-print(f"hola soy {Nombre} y tengo {edad} años ")
+#04-IMPRIMIENDO 2 VARIABLES EN UNA MISMA LINEA 
 
-Nombre="Duki"
+print('Hola mi nombre es',nombre,"y tengo",edad) #Impresion separando con comas
+print("Hola mi nombre es"+" "+name+" "+"y tengo"+str(edad)) #Concatenación con signo +
+print(f"Hola mi nombre es {nombre} y tengo {edad} años") #Formato de cadenas literales (f-string)
 
-print("Hola mi nuevo nombre es ",Nombre)
+##05-ACTUALIZANDO LA VARIABLE NOMBRE (Mutabilidad)
+nombre = "Duki"
+name = "William"
+print ("Hola mi nuevo nombre es", nombre)
+print ("Hola mi nuevo nombre es", name)
 
-Nombre=input("Escribe tu nombre ") 
+#06-¿VARIABLES EN UNA SOLA LINEA? (No es muy recomendable, solo en ciertas situaciones)
+ciudad, region, pais, year = "Puerto Montt", "Los Lagos", "Chile", 2010.
+print("Yo naci en la ciudad de", ciudad,", region de",region,pais,", el año",int(year))
 
-nombre1=input("Escribe tu otro nombre \n")
+#07-UTILIZANDO LA INSTRUCCIÓN INPUT
+nombre1 = input("¿Cuál es tu nombre?\n")
+edad1 = input("¿Cual es tu edad?\n")
 
-print("Tu nombre es ",nombre1)
+print("Tu nombre es:",nombre1,"y tu edad es", edad1)
 
-ciudad,region,pais,año="Pto montt","Los lagos","Chile",2010
+#08-ACOTACIÓN CONSTANTES
 
-
-
-
+PI = 3.14
+CIUDAD = "Osorno"
--- 02-tiposdatos.py
+++ 02-tiposdatos.py
@@ -1,115 +1,214 @@
+"""GUIA RÁPIDA TIPOS DE DATOS EN PYTHON
+Docente: Victor Saldivia Vera - Institución: Universidad de Los Lagos"""
 
-estatura= 1.70
+#01-DATOS DE TIPO NÚMERICO
+edad = 29 #entero
+estatura = 1.71 #real
+peso = 70.5 
+complejo =  1 + 4j #complejo
 
-edad=18
+print("######## 01-DATOS NÚMERICOS ########");
+print(f"Mi estatura es de {estatura} y mi peso es de {peso}")
+print("Impresion de un número complejo:",complejo,"\n")
 
-peso=68
+#Transformando un real a entero
+print(peso)
+print("Transformando un valor real a entero:",int(peso))
 
-complejo= 1 +4j
+#Transformando un entero a real
+print("Transformando un valor entero a real:",float(edad))
 
-print("Impresion del numero complejo: ",complejo)
+#OPERACION ARITMETICA BÁSICA
+imc = peso / (estatura**2)
+print(f"Mi IMC es de:{imc}\n")
 
-#Operacion Aritmetica base
+print("Mi IMC es de: {:.2f}".format(imc),"\n") #formateando el valor de salida a solo 2 decimales
 
-imc= peso/estatura**2
+#02-DATOS DE TIPO CADENA DE CARACTERES 
+asignatura = 'Programacion'
+carrera = "Ingenieria Civil en Informática"
+descripcion = '''La asignatura de programación se imparte en el primer semestre, tiene por objetivo
+entregar la base lógica para cualquier estudiante que comience a familiatizarse con la programación'''
 
-print("Mi IMC es de: ",imc)
+print("######## 02-STRINGS ########")
+print("La asignatura de",asignatura,"corresponde a la carrera de", carrera)
+print(descripcion[12])
 
-# Datos de tipo decadena
+#Utilizando la función len (cuenta la cantidad de caracteres)
+print("La cantidad de caracteres de la palabra", asignatura, "es de:",len(asignatura))
+print("La cantidad de caracteres de la palabra", carrera, "es de:",len(carrera),"\n")
 
-asignatura="Programacion"
+#03-VALORES BOOLEANOS
+ampolleta = False
+interruptor = True
 
-carrera="Ingenierica civil informatica"
+print("######## 03-BOOLEANS ########")
+print(ampolleta,interruptor)
+print("La variable ampolleta es de tipo:",type(interruptor),"\n") #Con type sabemos el tipo de datos con el cual estamos trabajando
 
-print(("La asignatura de ",asignatura,"es parte de la carrera de ",carrera))
+#Podemos transformar cualquier valor a un Booleano (al igual que un string, int, etc)
+print(bool(0))
+print(bool(""))
+print(bool([]))  
+print(bool("False"))
+print(bool(1)) 
+print("\n")
 
+#04-DATOS TIPO LIST (Objetos de Tipo Colección) - (Mutables)
+print("######## 04-LISTAS ########")
 
-
-#Vectores
-
-estudiantes=["Ignacio","Pedro" ,"Juan" ,"Alonso"]
-
-print(estudiantes)
-
-estatura=1.72
-peso=60
-complejo= 1+4j
-
-print ("Mi estatura es de {estatura} y mi peso es de {peso}")
-
-print("Impresion de un numero complejo")
-
-print("Transformar un valor real a antero",int(peso))
-
-print("Transformar entero a real",float(edad))
-
-imc=peso / (estatura**2)
-
-print("Mi mci es de: ",imc,"n/")
-
-
-print("El largo de la palabra",asignatura,"es de: ",len(asignatura))
-print("El largo de la palabra ",carrera,"Es de : ",len(carrera))
-
-ampolleta= False
-interruptor= True
-
-print("La variable ampolleta es de tipo ",type)
-
-nueva_lista = list()
-
-print("Esta es una lista vacia: ",nueva_lista)
-
-otra_lista = []
-
+#Inicializando listas de 2 maneras
+nueva_lista = list()              
+otra_lista = []                 
+print("Esta es una lista vacia:",nueva_lista)
+print("Esta es otra lista vacia:",otra_lista)
 print(type(otra_lista))
 
-estudiantes = ["Ricardo","Alonso","Rodolfo","Cristian"]
+#Declarando tres listas diferentes
+estudiantes = ["Matias", "Marco", "Cristobal", "Sebastián", "Marco"]
+num = [1,2,3,4,5,6,1]             
+lenguaje = list(["Python","Dart"])
 
-num = [1,2,3,4,5,6]
+# ¿Se puede realizar una lista de datos compuestos?
+data = ['Osorno', {'UV': 'nivel bajo', 'Temp °C': 15}, (-40.5725, -73.1353)]
+listamixta = ['Felipe', 100, True]
 
-lenguaje= ["python"]
+print("Lista de cadena de caracteres:",estudiantes)
+print("Lista de números:",num)
+print("Lista de un elemento:",lenguaje)
+print("Esta es una lista mixta:", listamixta)
+print("Esto igual es una lista:",data)
+print(len(listamixta)) #para saber la cantidad de elementos de una lista
+print(estudiantes.count("Marco"))   
+print(num.count(5000))  #cantidad de ocurrencias de un elemento en especifico dentro de la lista
 
-Lista_mixta = [str("xd"),int(1)]
+lenguaje = ["JavaScript"]
+print("Nuevo valor del Arreglo de un elemento:",lenguaje)
 
-print("La lista es : ",Lista_mixta)
+#¿Como accedo a un elemento especifico de la lista?
+print(estudiantes[0]) #correcto (1° elemento de la lista)
+print(estudiantes[1]) #(2° elemento de la lista)
+#print(estudiantes[6]) #incorrecto
+print("Posicion -2 de la lista estudiantes:",estudiantes[-2]) #impresión desde atras hacia adelante
 
-data = ['Osorno',{'UV': 'Nivel bajo' , 'Temp °C': 15},(-40.5725,-731353)]
-
-
-print(num.count(6))
-
-print(estudiantes[0])
-print(estudiantes[1])
-print("Posicion -2",estudiantes[-2])
-
+#Reasignando el valor de la 3° posicion de la lista
 estudiantes[3] = "Gabriela"
+print("El nuevo arreglo de estudiantes es:",estudiantes)
 
-data_asing = [10023,"Programacion",1,True]
+#Inicializando otra lista de datos mixtos
+data_asig = [10023,"Programación",1,True]
 
-cod,ramo,semestre,estado = data_asing
+#¿Que hace este código?    #Desempaquetando elementos de la lista (data_asig) a variables
+cod,ramo,semestre,estado = data_asig
+print(ramo)
 
-print (ramo)
+#¿Se pueden sumar listas?
+print("Suma de listas:",estudiantes + num)
 
-#Sumar listas
-
-lista1=[1,2,3,4,5,6,7,8,9,10]
-lista2=["con;",True,100]
-
-print(estudiantes+num)
+#¿Qué hacen estas funciones?
 print(list("Python"))
-print(list(range(10)))
+print(list(range(10))) #genera una lista 10 elementos
+print("\n")
 
+# > En el fichero de listas se mostrarán más funciones 
 
-grupo1=("Daniel","Cistian","Felipe",200,100,"Daniel")
-
+#05 - TUPLAS - (No mutables)
+newtupla = tuple()                     
+grupo1 = ("Daniel","Cristian","Felipe",200,100,"Daniel")
+print("######## 05-TUPLAS ########")
 print(type(grupo1))
 
+#Accediendo al primer elemento de la tupla
 print(grupo1[0])
 
-print(grupo1.count("Daniel"))
+#Consultando el elemento "Daniel" cuantas veces se encuentra en la tupla
+print("El elemento se repite:",grupo1.count("Daniel"))
 
-lista3=[1,2,3,4,5,6]
+#Muestra el indice del primer elemento buscado
+print("Indice del elemento:",grupo1.index ("Daniel"))
 
-for i in lista3:
-    print(i)
+#Reasignando el primer elemento de la tupla
+"""grupo1[0] = "Constanza"
+print(grupo1)"""
+
+#¿Se pueden sumar las tuplas?
+
+
+#Obteniendo un trozo de la tupla
+grupo2 = ("Pedro",100,"Felipe","Diego",2020,"Alejandra")
+print("Trozo de la tupla",grupo2[0:3])
+
+#¿Entonces como no puedo modificar una tupla, que puedo hacer?
+grupo1 = list(grupo1)
+print("La tupla ahora es de tipo:",type(grupo1),"\n")
+print("\n")
+
+#06 - SETS (Conjuntos) -  Estructura fija
+#Formas de inicializar un Set
+print("######## 06-SETS ########")
+conjunto_vacio = set()           
+conjunto_vacio1 = {}  #¿diccionario o set?
+print(type(conjunto_vacio1))
+conjunto_colores = set({"Azul","Rojo","Verde"}) #utilizando la funcion set
+conjunto_animales = {"Gato","Perro","Loro"}     #utilizando llaves
+
+print(type(conjunto_colores)) #tipo de dato set
+print(type(conjunto_animales)) #tipo de dato set
+print("El primer set contiene los siguientes colores:",conjunto_colores)
+print("El segundo set contiene los siguientes animales:",conjunto_animales)
+
+#print(conjunto_animales[0]) #accediendo al primer elemento del set
+conjunto_colores.add("Celeste")
+print("El set de colores lo conforman:",conjunto_colores)  #un set es una estructura desordenada a diferencia de una Lista
+
+#conjunto_animales.add("Gato")
+print("El set de animales lo conforman:",conjunto_animales,"\n") #un set no acepta duplicados, a diferencia de las listas que si.
+
+
+#07 - DICCIONARIOS (Clave-Valor)
+print("######## 06-DICCIONARIOS ########")
+
+diccionario1 = dict()
+diccionario2 = {}
+
+datos_personales = {
+    "Nombre":"Victor",
+    "Institucion":"Ulagos",
+    "Edad":29
+    }
+
+print(datos_personales)
+
+datos_personales = {
+    "Nombre":"Victor",
+    "Institucion":"Ulagos",
+    "Edad": 29,
+    "Asignaturas": {"Estructura de Datos", "Programación"}
+    }
+
+print("Diccionario inicial:",datos_personales)
+
+#Consulta la cantidad de elementos del Diccionario
+print(len(datos_personales))
+
+#Es facilmente accesible a los elementos dentro de un diccionario
+print(datos_personales["Institucion"])
+
+#¿Como actualizamos el valor de una clave dentro de un diccionario?
+datos_personales["Institucion"] = "USS" 
+print("Diccionario actualizado:",datos_personales)
+
+#Agregando un nuevo clave al diccionario
+datos_personales["Ciudad"] = "Osorno"
+print(datos_personales)
+print("Diccionario con el nuevo campo:",datos_personales)
+
+#Eliminando un campo del diccionario
+del datos_personales["Ciudad"]
+print("Diccionario con el campo eliminado:",datos_personales)
+
+hospital = dict(
+    nombre = "Hospital San Jose",
+    direccion = "Dr. Guillermo Buhler 1765",
+    ciudad = "Osorno"
+)
